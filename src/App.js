d19968f26b88919faf2655e1b5035e62e7062a08
mutasem <motasemimad10@gmail.com>
2023-03-02 14:12:08 +0200

    setup the pages and components and default style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import Navbar from './components/Navbar';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
-  return <div className='App'>Hi React</div>;
+  return (
+    <Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 }
 
 export default App;
